a51b5f8bc8e1d124f7ab4b7787e3ab8ce2de6ab9
Diivvuu <divyanshubawa9@gmail.com>
2024-06-21 17:36:25 +0530

    custom hook final

--- src/hooks/useFormHook.js
+++ src/hooks/useFormHook.js
@@ -15,9 +15,31 @@ const useFormHook = (initialFormData, validate) => {
         ...formData,
         addSkills: [...formData.addSkills, value],
       });
+    } else {
+      setFormData({
+        ...formData,
+        addSkills: formData.addSkills.filter((skill) => skill !== value),
+      });
     }
   };
-  return <div></div>;
+  const validateForm = () => {
+    const errors = validate(formData);
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      console.log(formData);
+    }
+  };
+  return {
+    formData,
+    formErrors,
+    handleInputChange,
+    handleCheckBoxChange,
+    handleSubmit,
+  };
 };
 
 export default useFormHook;
